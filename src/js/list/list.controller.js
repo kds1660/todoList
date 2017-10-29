ba6ca2c25d08e69a8a6a774d71eead513f32ae96
Kovaluov Dmitriy <kds16061985@gmail.com>
2017-10-29 09:46:01 +0200

    change error response in list controller, reformat code

--- src/js/list/list.controller.js
+++ src/js/list/list.controller.js
@@ -1,119 +1,127 @@
-listModule.controller('listController', ['$scope', '$rootScope', 'toastr', 'moment', 'listServices', 'loginServices', function ($scope, $rootScope, toastr, moment, listServices, loginServices) {
-    $scope.list = {
-        done: [],
-        todo: []
-    };
-
-    listServices.apiRequest.get().$promise.then(function (response) {
-        for (var i = 0; i < response.length; i++) {
-            response[i].date=listServices.toLocalTime(response[i].date);
-            if (response[i].executed) {
-                $scope.list.done.push(response[i]);
-            } else {
-                $scope.list.todo.push(response[i]);
-            }
-        }
-    }, function (response) {
-    });
-
-    $scope.executeTask = function ($index) {
-        listServices.apiRequest.save({
-            task: $scope.list.todo[$index].id,
-            execute: 1
-        }).$promise.then(function (response) {
-            toastr.success(response.message, 'Success');
-            $scope.list.todo[$index].executed = true;
-            $scope.list.done.push($scope.list.todo[$index]);
-            $scope.list.todo.splice($index, 1);
-        }, function (response) {
-            toastr.error(response.message, 'Error');
-        });
-    };
-
-    $scope.removeTask = function ($index) {
-        listServices.apiRequest.delete({task: $scope.list.done[$index].id}).$promise.then(function (response) {
-            toastr.success(response.message, 'Success');
-            $scope.list.done.splice($index, 1);
-        }, function (response) {
-            toastr.error(response.message, 'Error');
-        });
-    };
-
-    $scope.restoreTask = function ($index) {
-        listServices.apiRequest.save({
-            task: $scope.list.done[$index].id,
-            execute: 0
-        }).$promise.then(function (response) {
-            toastr.success(response.message, 'Success');
-            $scope.list.done[$index].executed = false;
-            $scope.list.todo.push($scope.list.done[$index]);
-            $scope.list.done.splice($index, 1)
-        }, function (response) {
-            toastr.error(response.message, 'Error');
-        });
-    };
-
-    $scope.addTask = function () {
-        var newTask = {
-            text: angular.element(document.querySelector('#task')).val(),
-            date: new Date()
+listModule.controller('listController', ['$scope', '$rootScope', 'toastr', 'moment', 'listServices',
+    function ($scope, $rootScope, toastr, moment, listServices) {
+        $scope.list = {
+            done: [],
+            todo: []
         };
-        listServices.apiRequest.save(newTask).$promise.then(function (response) {
-            newTask.id = response.id;
-            newTask.date=listServices.toLocalTime(newTask.date);
-            $scope.list.todo.push(newTask);
-            angular.element(document.querySelector('#task')).val('');
-            toastr.success(response.message, 'Success');
-        }, function (response) {
-            toastr.error(response.message, 'Error');
-        });
 
-    };
-
-    $scope.editTask = function ($index) {
-        //show modal window for action
-        $scope.editText = {
-            task: $scope.list.todo[$index].id,
-            text:$scope.list.todo[$index].text
-        };
-        listServices.editElement($scope).then(function () {
-            listServices.apiRequest.update($scope.editText).$promise.then(function (response) {
-                toastr.success(response.message, 'Success');
-                $scope.list.todo[$index].text=$scope.editText.text;
-            }, function (response) {
-                toastr.error(response.message, 'Error');
-            });
-        }, function () {
-        });
-    };
-
-    $scope.filterList = function () {
-        var filterValue = angular.element(document.querySelector('.filter')).val();
-
-        if (filterValue !== '') {
-            angular.forEach(angular.element(document.querySelectorAll('li')), function (elm) {
-                var text = elm.querySelector('span').innerText;
-
-                if (text.substr(0, filterValue.length) !== filterValue) {
-                    angular.element(elm).css('display', 'none');
+        listServices.apiRequest.get().$promise.then(function (response) {
+            for (var i = 0; i < response.length; i++) {
+                response[i].date = listServices.toLocalTime(response[i].date);
+                if (response[i].executed) {
+                    $scope.list.done.push(response[i]);
                 } else {
-                    angular.element(elm).css('display', 'block')
+                    $scope.list.todo.push(response[i]);
                 }
-            })
-        } else {
-            angular.element(document.querySelectorAll('li')).css('display', 'block');
-        }
-    };
-
-    $scope.sortList = function (value) {
-        function sortByProp (value) {
-            return function (a,b) {
-                if (a[value]<b[value]) {return -1}
-                if (a[value]>b[value]) {return 1}
-                return 0
             }
+        }, function (response) {
+        });
+
+        $scope.executeTask = function ($index) {
+            listServices.apiRequest.save({
+                task: $scope.list.todo[$index].id,
+                execute: 1
+            }).$promise.then(function (response) {
+                toastr.success(response.message, 'Success');
+                $scope.list.todo[$index].executed = true;
+                $scope.list.done.push($scope.list.todo[$index]);
+                $scope.list.todo.splice($index, 1);
+            }, function (response) {
+                toastr.error(response.data.message, 'Error');
+            });
+        };
+
+        $scope.removeTask = function ($index) {
+            listServices.apiRequest.delete({task: $scope.list.done[$index].id}).$promise.then(function (response) {
+                toastr.success(response.message, 'Success');
+                $scope.list.done.splice($index, 1);
+            }, function (response) {
+                toastr.error(response.data.message, 'Error');
+            });
+        };
+
+        $scope.restoreTask = function ($index) {
+            listServices.apiRequest.save({
+                task: $scope.list.done[$index].id,
+                execute: 0
+            }).$promise.then(function (response) {
+                toastr.success(response.message, 'Success');
+                $scope.list.done[$index].executed = false;
+                $scope.list.todo.push($scope.list.done[$index]);
+                $scope.list.done.splice($index, 1)
+            }, function (response) {
+                toastr.error(response.data.message, 'Error');
+            });
+        };
+
+        $scope.addTask = function () {
+            var newTask = {
+                text: angular.element(document.querySelector('#task')).val(),
+                date: new Date()
+            };
+            listServices.apiRequest.save(newTask).$promise.then(function (response) {
+                newTask.id = response.id;
+                newTask.date = listServices.toLocalTime(newTask.date);
+                $scope.list.todo.push(newTask);
+                angular.element(document.querySelector('#task')).val('');
+                toastr.success(response.message, 'Success');
+            }, function (response) {
+                toastr.error(response.data.message, 'Error');
+            });
+
+        };
+
+        $scope.editTask = function ($index) {
+            //show modal window for action
+            $scope.editText = {
+                task: $scope.list.todo[$index].id,
+                text: $scope.list.todo[$index].text
+            };
+            listServices.editElement($scope).then(function () {
+                listServices.apiRequest.update($scope.editText).$promise.then(function (response) {
+                    toastr.success(response.message, 'Success');
+                    $scope.list.todo[$index].text = $scope.editText.text;
+                }, function (response) {
+                    toastr.error(response.data.message, 'Error');
+                });
+            }, function () {
+            });
+        };
+
+        $scope.filterList = function () {
+            var filterValue = angular.element(document.querySelector('.filter')).val();
+
+            if (filterValue !== '') {
+                angular.forEach(angular.element(document.querySelectorAll('li')), function (elm) {
+                    var text = elm.querySelector('span').innerText;
+
+                    if (text.substr(0, filterValue.length) !== filterValue) {
+                        angular.element(elm).css('display', 'none');
+                    } else {
+                        angular.element(elm).css('display', 'block')
+                    }
+                })
+            } else {
+                angular.element(document.querySelectorAll('li')).css('display', 'block');
+            }
+        };
+
+        $scope.sortList = function (value) {
+            function sortByProp(value) {
+                return function (a, b) {
+
+                    if (a[value] < b[value]) {
+                        return -1
+                    }
+
+                    if (a[value] > b[value]) {
+                        return 1
+                    }
+                    return 0
+                }
+            }
+
+            $scope.list.done.sort(sortByProp(value));
+            $scope.list.todo.sort(sortByProp(value));
         }
-        $scope.list.done.sort(sortByProp(value));
-        $scope.list.todo.sort(sortByProp(value));
-    }
-}]);
+    }]);
--- src/js/login/login.controller.js
+++ src/js/login/login.controller.js
@@ -1,46 +1,48 @@
-loginModule.controller('loginController', ['$scope', '$rootScope', '$state', 'toastr', 'loginServices', function ($scope, $rootScope, $state, toastr, loginServices) {
-    $scope.loginUser = {};
+loginModule.controller('loginController', ['$scope', '$rootScope', '$state', 'toastr', 'loginServices',
+    function ($scope, $rootScope, $state, toastr, loginServices) {
+        $scope.loginUser = {};
 
-    loginServices.apiRequest.get({user: 'logged'}).$promise.then(function (response) {
-        loginServices.setUser(response.message);
-        $scope.userName = loginServices.getUser();
+        loginServices.apiRequest.get({user: 'logged'}).$promise.then(function (response) {
+            loginServices.setUser(response.message);
+            $scope.userName = loginServices.getUser();
 
-        if (loginServices.getUser()) {
-            $state.go('logged');
-        } else {
-            $state.go('login');
-        }
-    });
-
-    $scope.login = function () {
-        loginServices.apiRequest.save($scope.loginUser).$promise.then(function (response) {
-            toastr.success(response.message, 'Success');
-            $state.go('logged');
-
-        }, function (response) {
-            toastr.error(response.data.message, 'Error');
+            if (loginServices.getUser()) {
+                $state.go('logged');
+            } else {
+                $state.go('login');
+            }
         });
-    };
 
-    $scope.register = function () {
-
-        if (!$scope.loginUser.name || !$scope.loginUser.password) {
-            toastr.error('Set name/password', 'Error');
-        } else {
-            loginServices.apiRequest.update($scope.loginUser).$promise.then(function (response) {
+        $scope.login = function () {
+            loginServices.apiRequest.save($scope.loginUser).$promise.then(function (response) {
                 toastr.success(response.message, 'Success');
+                $state.go('logged');
+
+            }, function (response) {
+                toastr.error(response.data.message, 'Error');
+            });
+        };
+
+        $scope.register = function () {
+
+            if (!$scope.loginUser.name || !$scope.loginUser.password) {
+                toastr.error('Set name/password', 'Error');
+            } else {
+                loginServices.apiRequest.update($scope.loginUser).$promise.then(function (response) {
+                    toastr.success(response.message, 'Success');
+                }, function (response) {
+                    toastr.error(response.data.message, 'Error');
+                })
+            }
+        };
+
+        $scope.logout = function () {
+            loginServices.apiRequest.get({user: 'logout'}).$promise.then(function (response) {
+                toastr.success(response.message, 'Success');
+                loginServices.setUser('');
+                $state.go('login');
             }, function (response) {
                 toastr.error(response.data.message, 'Error');
             })
-        }
-    };
-    $scope.logout = function () {
-        loginServices.apiRequest.get({user: 'logout'}).$promise.then(function (response) {
-            toastr.success(response.message, 'Success');
-            loginServices.setUser('');
-            $state.go('login');
-        }, function (response) {
-            toastr.error(response.data.message, 'Error');
-        })
-    };
-}]);
+        };
+    }]);
